a7f8c82954ba140e96079b81e6328a811c114537
Manel Puig <manel.puig@ub.edu>
2021-08-28 14:08:59 +0200

    Added laserscan

--- src/rubot_control/src/lidar_test.py
+++ src/rubot_control/src/lidar_test.py
@@ -4,17 +4,18 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print len(msg.ranges)
+    print ("Number of scan points: "+ str(len(msg.ranges)))
     # values at 0 degree
-    print msg.ranges[0]
+    #print ("Distance at 0º: " + str(msg.ranges[0]))
+    print (msg.ranges[0])
     # values at 90 degree
-    print msg.ranges[90]
+    #print ("Distance at 90º: " + str(msg.ranges[90]))
     # values at 180 degree
-    print msg.ranges[180]
+    #print ("Distance at 180º: " + str(msg.ranges[180]))
     # values at 270 degree
-    print msg.ranges[270]
+    #print ("Distance at 270º: " + str(msg.ranges[270]))
     # values at 360 degree
-    print msg.ranges[359]
+    #print ("Distance at 360º: " + str(msg.ranges[359]))
 
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('/scan', LaserScan, callback)
