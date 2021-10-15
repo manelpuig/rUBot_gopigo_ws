2bffe4fd69bbb27267a1162099981ec0a399c7bc
manelpuig <manel.puig@ub.edu>
2021-10-15 13:57:28 +0200

    Corrections SLAM and project

--- src/vision/src/go_to_specific_point_on_map.py
+++ src/vision/src/go_to_specific_point_on_map.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
         navigator = GoToPose()
 
         # Customize the following values so they are appropriate for your location
-        position = {'x': 1.22, 'y' : 2.56}
+        position = {'x': -0.5, 'y' : -0.8}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
 
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
